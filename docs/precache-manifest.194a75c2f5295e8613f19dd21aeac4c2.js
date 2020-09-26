b6dd4fcaa0bf94443d7ad62e1750ad517d0b74b2
Adalberto Roque <adalberto.roque@outlook.com>
2020-09-25 21:06:16 -0500

    desplegar docs

---
+++ docs/precache-manifest.194a75c2f5295e8613f19dd21aeac4c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117a90dc950b365dead453fc65046517",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc48bcb6c0e4ebca3b76",
+    "url": "/static/css/main.0e5a9d4e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc48bcb6c0e4ebca3b76",
+    "url": "/static/js/main.3b3a15a1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
